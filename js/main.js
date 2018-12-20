82cb335a885e4b657c693fc63c5dd5019cd0f1c0
Mirehiko <mirehiko@localhost.localdomain>
2018-12-20 07:31:26 +0500

    main links in menu, close menu by click outer menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 $('#header-menu').on('click', function(e) {
+	e.stopPropagation();
+
 	$('.side-header').toggleClass('open');
 	$('.backlayer').toggleClass('-visible-');
 
 	if ( $('.side-header').hasClass('open') ) {
-		console.log('open')
+		// console.log('open')
 		$('.side-header .line.is-b').css('display', 'none');
 	} else {
-		console.log('close')
+		// console.log('close')
+		$('.side-header .line.is-b').css('display', 'block');
+	}
+});
+
+$('.side-header').on('click', function(e) {
+	if ( $('.side-header').hasClass('open') ) {
+		// console.log('close')
+		$('.side-header').removeClass('open');
+		$('.backlayer').removeClass('-visible-');
 		$('.side-header .line.is-b').css('display', 'block');
 	}
 });
