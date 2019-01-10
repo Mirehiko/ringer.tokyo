060223ddc67aebb91e71395f634b944c4ea79382
mirehiko <mirehiko@gmail.com>
2019-01-10 12:18:34 +0500

    отрисовка подготовленных данных

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@ var textData = [
 		categoty: '',
 		preview: {
 			type: 'image',
-			src: 'image/deki2_2.jpg',
+			src: 'images/works/deki2_2.jpg',
 		},
-		src: 'image/deki2_2.jpg',
+		src: 'images/works/deki2_2.jpg',
 		link: 'works.html',
 	},
 	{
@@ -16,9 +16,9 @@ var textData = [
 		categoty: '',
 		preview: {
 			type: 'image',
-			src: 'image/deki2_2.jpg',
+			src: 'images/works/deki2_4.jpg',
 		},
-		src: 'image/deki2_2.jpg',
+		src: 'images/works/deki2_4.jpg',
 		link: 'works.html',
 	},
 	{
@@ -27,9 +27,9 @@ var textData = [
 		categoty: '',
 		preview: {
 			type: 'image',
-			src: 'image/deki2_2.jpg',
+			src: 'images/works/deki2_6.jpg',
 		},
-		src: 'image/deki2_2.jpg',
+		src: 'images/works/deki2_6.jpg',
 		link: 'works.html',
 	},
 	{
@@ -38,49 +38,109 @@ var textData = [
 		categoty: '',
 		preview: {
 			type: 'image',
-			src: 'image/deki2_2.jpg',
+			src: 'images/works/deki2_8.jpg',
 		},
-		src: 'image/deki2_2.jpg',
+		src: 'images/works/deki2_8.jpg',
 		link: 'works.html',
 	},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
-	{},
+	{
+		title: 'testTitle',
+		lauch: '',
+		categoty: '',
+		preview: {
+			type: 'image',
+			src: 'images/works/victas_iamnext_03.jpg',
+		},
+		src: 'images/works/victas_iamnext_03.jpg',
+		link: 'works.html',
+	},
+	{
+		title: 'testTitle',
+		lauch: '',
+		categoty: '',
+		preview: {
+			type: 'image',
+			src: 'images/works/iamnext.jpg',
+		},
+		src: 'images/works/iamnext.jpg',
+		link: 'works.html',
+	},
 ];
 
-renderPage(textData)
+Array.prototype.shuffle = function() {
+  for (var i = this.length - 1; i > 0; i--) {
+    var num = Math.floor(Math.random() * (i + 1));
+    var d = this[num];
+    this[num] = this[i];
+    this[i] = d;
+  }
+  return this;
+}
+
+
+$('#homePage').append(renderPage(textData));
 
 function renderPage(data) {
 	let tmpData = data;
 	let result = document.createElement('div');
+	$(result).addClass('infiniteBox');
 	let linesCount = data.length;
-	console.log('linesCount',linesCount)
+	console.log('linesCount:',linesCount)
 
+	// Вычисляем какие плитки будут
 	let lines = calcView(linesCount);
-	console.log('lines',lines)
+	console.log('lines:',lines)
 
+	// Отбираем данные для тройной плитки
 	let tripleLines = lines.filter(length => length == 3);
-	console.log('tripleLines',tripleLines)
+	let tripleData = [];
 	let lineTypes = [];
-	for (item of tripleLines) {
-		lineTypes.push(autoSelectType())
-	}
-	console.log('lineTypes',lineTypes)
 
-	let resultLines = [];
-	
+	if (tripleLines.length) {
+		// Для линий с тремя объектами вычисляем тип плитки
+		for (item of tripleLines) {
+			lineTypes.push(autoSelectType())
+		}
+		console.log('lineTypes:',lineTypes)
+
+		// Отбираем данные для отображения
+		for (let i = 0; i < tripleLines.length; i++ ) {
+			tripleData.push({
+				lineType: lineTypes[i],
+				data: [].concat(tmpData.splice(0, tripleLines[i])),
+			});
+		}
+	}
+
+	// Отбираем данные для двойной плитки
+	let doubleLines = lines.filter(length => length == 2);
+	let doubleData = [];
+
+	if ( doubleLines.length ) {
+		// Отбираем данные для отображения
+		for (item of doubleLines) {
+			doubleData.push(tmpData.splice(0, item));
+		}
+	}
+
+	// Собираем все плитки воедино
+	let resultLines = doubleData.concat(tripleData);
+	console.log('resultLines:',resultLines)
+	resultLines.shuffle(); // Задаем случайное положение
+	console.log('resultLines:',resultLines)
+
+	// Собираем данные для отрисовки
+	for ( item of resultLines ) {
+		if ( item.length == 2 ) {
+			$(result).append(doubleLine(item));
+		} else {
+			$(result).append(item.lineType(item.data));
+		}
+	}
+	console.log('result:',result)
+
+
+	return result;
 }
 
 function autoSelectType() {
@@ -88,15 +148,15 @@ function autoSelectType() {
 	let type = null;
 	switch (typeID) {
 		case 1: {
-			type = 'triple';
+			type = tripleLine;
 			break;
 		}
 		case 2: {
-			type = 'tsob';
+			type = tsobLine;
 			break;
 		}
 		case 3: {
-			type = 'obts';
+			type = obtsLine;
 			break;
 		}
 		default:
@@ -255,9 +315,9 @@ function drawItem(data) {
 
 	let back = document.createElement('div');
 	$(back).addClass('itemBack');
-	$(back).css('background-image', data.src);
+	$(back).css('background-image', 'url('+data.src+')');
 	$(back).css('background-size', 'cover');
-	$(back).css('background-repeate', 'no-repeat');
+	$(back).css('background-repeat', 'no-repeat');
 	$(item).append(back);
 
 	let itemContent = document.createElement('div');
@@ -267,15 +327,18 @@ function drawItem(data) {
 
 	let itemTitle = document.createElement('div');
 	$(itemTitle).addClass('itemTitle');
-	$(itemContent).append(itemContent);
+	$(itemTitle).text(data.title);
+	$(itemContent).append(itemTitle);
 
 	let itemLauchDate = document.createElement('div');
 	$(itemLauchDate).addClass('itemLauchDate');
-	$(itemContent).append(itemContent);
+	$(itemLauchDate).text(data.lauch);
+	$(itemContent).append(itemLauchDate);
 
 	let itemCategory = document.createElement('div');
 	$(itemCategory).addClass('itemCategory');
-	$(itemContent).append(itemContent);
+	$(itemCategory).text(data.category);
+	$(itemContent).append(itemCategory);
 
 
 	let previewBlock = document.createElement('div');
