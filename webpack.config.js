e35d69788a1d68335961c29c96cedcf8bb788569
mirehiko <mirehiko@gmail.com>
2019-02-12 17:16:12 +0500

    replace files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path')
+const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 
 module.exports = {
+	context: path.resolve(__dirname, 'src'),
 	entry: {
-		app: './src/index.js',
+		app: './js/index.js',
 	},
 	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, './dist'),
-		publicPath: '/dist'
+		filename: 'js/[name].js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '../'
 	},
 	module: {
 		rules: [{
@@ -39,3 +41,46 @@ module.exports = {
 		})
 	]
 }
+
+// const path = require('path')
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//
+//
+// module.exports = {
+// 	entry: {
+// 		app: './src/index.js',
+// 	},
+// 	output: {
+// 		filename: '[name].js',
+// 		path: path.resolve(__dirname, './dist'),
+// 		publicPath: '/dist'
+// 	},
+// 	module: {
+// 		rules: [{
+// 			test: /\.js$/,
+// 			loader: 'babel-loader',
+// 			exclude: '/node_modules/'
+// 		}, {
+// 			test: /\.css$/,
+// 			use: [
+// 				MiniCssExtractPlugin.loader,
+// 				{
+// 					loader: "css-loader",
+// 					options: { sourceMap: true }
+// 				}, {
+// 					loader: "postcss-loader",
+// 					options: { sourceMap: true, config: { path: 'src/js/postcss.config.js' } }
+// 				},
+// 			]
+// 		}]
+// 	},
+// 	devServer: {
+// 		overlay: true
+// 	},
+// 	plugins: [
+// 		new MiniCssExtractPlugin({
+// 			filename: "[name].css"
+// 		})
+// 	]
+// }
+//
