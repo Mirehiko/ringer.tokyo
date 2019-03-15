d4eb1fd9606eb6e524be0bd8ea513e4a2c7bfcdc
mirehiko <mirehiko@gmail.com>
2019-03-15 16:15:25 +0500

    static files settings.py

--- ringo/ringo/settings.py
+++ ringo/ringo/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'a45l&potp%d#ota&-o)#xz*_t@*yh%de0$vk%gml%uzk)&in2^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -104,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Yekaterinburg'
 
@@ -118,4 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+# STATIC_VERSION = '20181211001'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), '/var/www/static/')
