f74e1518b50ed2f4000d1996054a50d16c158b64
Mirehiko <mirehiko@localhost.localdomain>
2020-02-09 11:54:49 +0500

    remove data

--- build/ringo/ringo/settings.py
+++ build/ringo/ringo/settings.py
@@ -195,8 +195,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.mail.ru'
 EMAIL_PORT = 2525
-EMAIL_HOST_USER = "car-tube@mail.ru"
-EMAIL_HOST_PASSWORD = "motor911"
+EMAIL_HOST_USER = "******"
+EMAIL_HOST_PASSWORD = "******"
 EMAIL_USE_TLS = True
 
 SERVER_EMAIL = EMAIL_HOST_USER
